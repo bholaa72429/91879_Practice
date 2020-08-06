5cd3cf24ea903adc796a7589093226994c609575
bholaa72429 <58008527+bholaa72429@users.noreply.github.com>
2020-08-07 10:54:22 +1200

    Database Connect (part 1)

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+ini_set('display_error','On'); // change to 'Off'when live
+
+define ("DB_NAME","bholaa72429");
+define ("DB_USERNAME","_bholaa72429");
+define ("DB_PASSWORD","f75b4bc51877b7704997c3fc28a4d7ff");
+define ("DB_HOST","localhost");
+
+?>
