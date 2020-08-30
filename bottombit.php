ef421dea02a9d43cb2739200fbb2951cff903f96
bholaa72429 <58008527+bholaa72429@users.noreply.github.com>
2020-08-31 09:22:20 +1200

    post covid

--- bottombit.php
+++ bottombit.php
@@ -1,8 +1,22 @@
+  <div class="box side">
+        
+        <h2>Search | <a href="showall.php">Show All</a></h2>
+            
+        <i>Type part of the title / author name if desired</i>
+            
+        <hr />
+            
+        Title Search<br />
+        Author Search<br />
+        Genre Search<br />
+        Rating Search
+            
+</div>    <!-- / side bar -->
 
-        <div class="box footer">
-            CC Anshika 2020
-        </div>    <!-- / footer -->
+<div class="box footer">
+    CC Anshika 2020
+</div>    <!-- / footer -->
     
-    </div>  <!-- / wrapper  -->
+</div>  <!-- / wrapper  -->
     
 </body>        
